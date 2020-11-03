87f8719dc87233208a74d570acda8bd03e26b106
nirav6 <nirav@alfangroup.com>
2020-11-02 22:26:20 -0500

    project code ir

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+export default function Form() {
+  return (
+    <form>
+      <label htmlFor="owner">Owner</label>
+      <input type="text" name="owner" id="owner" />
+      <label htmlFor="description">Description</label>
+      <input type="text" name="description" id="description" />
+      <input type="button" value="Add New Cat" />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
